0fa87f35ae56c25420b485e52f98c72b8de246b2
bmshema <92175742+bmshema@users.noreply.github.com>
2023-07-26 12:16:51 -0400

    Add filename variable prompts

--- twoPlaces.sh
+++ twoPlaces.sh
@@ -7,15 +7,20 @@
 # Resulting output is in bothplaces.txt numbered by how many locations a
 # MAC address was present in 
 
-tshark -r file1.pcap -Y wlan.addr -T fields -e wlan.sa > location1.txt
-tshark -r file1.pcap -Y wlan.addr -T fields -e wlan.ta >> location1.txt
-tshark -r file1.pcap -Y wlan.addr -T fields -e wlan.ra >> location1.txt
-tshark -r file1.pcap -Y wlan.addr -T fields -e wlan.da >> location1.txt
+echo "PCAP filename from location 1: "
+read LOC1
+echo "PCAP filename from location 2: "
+read LOC2
 
-tshark -r file2.pcap -Y wlan.addr -T fields -e wlan.sa > location2.txt
-tshark -r file2.pcap -Y wlan.addr -T fields -e wlan.ta >> location2.txt
-tshark -r file2.pcap -Y wlan.addr -T fields -e wlan.ra >> location2.txt
-tshark -r file2.pcap -Y wlan.addr -T fields -e wlan.da >> location2.txt
+tshark -r $LOC1 -Y wlan.addr -T fields -e wlan.sa > location1.txt
+tshark -r $LOC1 -Y wlan.addr -T fields -e wlan.ta >> location1.txt
+tshark -r $LOC1 -Y wlan.addr -T fields -e wlan.ra >> location1.txt
+tshark -r $LOC1 -Y wlan.addr -T fields -e wlan.da >> location1.txt
+
+tshark -r $LOC2 -Y wlan.addr -T fields -e wlan.sa > location2.txt
+tshark -r $LOC2 -Y wlan.addr -T fields -e wlan.ta >> location2.txt
+tshark -r $LOC2 -Y wlan.addr -T fields -e wlan.ra >> location2.txt
+tshark -r $LOC2 -Y wlan.addr -T fields -e wlan.da >> location2.txt
 
 cat location1.txt | sort | uniq > location1_uniq.txt
 cat location2.txt | sort | uniq > location2_uniq.txt
