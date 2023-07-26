f864b0b82dc22a5bce83b4a7c690939c3e329153
bmshema <92175742+bmshema@users.noreply.github.com>
2023-07-26 14:58:24 -0400

    Strip probe request ssids from each location and dedupe

--- twoPlaces_SSID.sh
+++ twoPlaces_SSID.sh
@@ -10,3 +10,7 @@ echo "PCAP filename from location 1: "
 read LOC1
 echo "PCAP filename from location 2: "
 read LOC2
+
+tshark -r $LOC1 -Y wlan.addr -T fields -e wlan.ta >> location1.txt
+
+tshark -r $LOC2 -Y wlan.addr -T fields -e wlan.ta >> location2.txt
